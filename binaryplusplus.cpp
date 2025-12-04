d14f40ba4ca8cd5f0755b10c9b4faf0fcdf688c3
Fergut11 <fergut11@mail.lcc.edu>
2025-12-04 09:00:11 -0500

    Update binaryplusplus.cpp
    Fixed issues converting 0 to binary.
    In the function that strips leading 0s it was originally checking for pointer literals and not the character 0 itself.
    The return for that function also returned 0 as a number rather than a character.
    
    Also tidied up and made a nice intro.

--- binaryplusplus.cpp
+++ binaryplusplus.cpp
@@ -10,7 +10,14 @@ int get_number(){
     string user_input;
     int binary_result;
 
-    cout << "Type a number and it will return as a binary"<< endl;
+    cout << R"(
+    Type a number and it will return as a binary
+    Decimal can be as written, Hex should be preceeded with a 0x, Octal with a 0.
+    Dec : 5, 65, 76
+    Hexidecimal : 0xFF43, 0x10F7, 0xABC123
+    Octodecimal : 0771, 0233, 044
+    
+    )";
     while (true) {
         cin >> user_input;
         try {
@@ -32,10 +39,12 @@ int get_number(){
 // the substr will only show the bits after the fird found 1
 string convert_to_binary(int number){
     string bitset_string = bitset<32>(number).to_string();
-    size_t first_one = bitset_string.find_first_not_of("0");
+    // make sure the '0' is not "0" or it will crash
+    // "0" is a pointer to a string literal not the character itself
+    size_t first_one = bitset_string.find_first_not_of('0');
 
     if (first_one == string::npos){
-        return 0;
+        return "0";
     }
 
     return bitset_string.substr(first_one);
@@ -56,6 +65,8 @@ char* terminal_conversion(char* terminal_input){
 
 // prints out the number input from user converted to binary
 int main(int argc, char* argv[]) {
+    // argc checks if an input was given through the terminal or not
+    // if not the program runs a little differently
     if (argc > 1) {
        cout << terminal_conversion(argv[1]);
     }
