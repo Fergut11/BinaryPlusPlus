f3f70a1225bc0bf01fc6e26c21cab9bde708c88f
Fergut11 <fergut11@mail.lcc.edu>
2025-12-04 06:20:01 -0500

    Update binaryplusplus.cpp
    It should now take in a number as an int and convert it to binary.
    It will deal with bad inputs like strings and request a correct number. Does not support hex or any other type of number besides decimal.

--- binaryplusplus.cpp
+++ binaryplusplus.cpp
@@ -1,7 +1,26 @@
+#include <bitset>
 #include <iostream>
 using namespace std;    
 
+
+int get_number(){
+    int number;
+    cout << "Type a number and it will return as a binary"<< endl;
+    while (true) {
+        if (cin >> number){
+            break;
+        }
+        else {
+            cout << "Please input a valid number"<< endl;
+            // cin.clear() gets rid of the user's input
+            // cin.ignore() removes the input from the buffer, well it removes 10000 characters 
+            cin.clear();
+            cin.ignore(10000, '\n');
+        }
+    }
+    return number;
+}
 int main() {
-    cout << "Hello, world!" << endl;
+    cout << bitset<32>(get_number());
     return 0;
 }
