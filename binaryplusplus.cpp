42cdf084f29be8cfabf8a452ef457a395c18dda9
Fergut11 <fergut11@mail.lcc.edu>
2025-12-04 07:57:27 -0500

    Update binaryplusplus.cpp
    Should now work with hex, decimal, octal, etc.
    Takes a string as an input, converts it to an int using stoi which allows input from any base.

--- binaryplusplus.cpp
+++ binaryplusplus.cpp
@@ -1,24 +1,28 @@
 #include <bitset>
 #include <iostream>
+#include <string>
 using namespace std;    
 
 
 int get_number(){
-    int number;
+    string user_input;
+    int binary_result;
+
     cout << "Type a number and it will return as a binary"<< endl;
     while (true) {
-        if (cin >> number){
+        cin >> user_input;
+        try {
+            // this takes user_input and converts it to an int
+            // nullptr means to not care about pointers
+            // the 0 denotes the base and when 0 it should auto detect
+            binary_result = stoi(user_input,nullptr, 0);
             break;
         }
-        else {
-            cout << "Please input a valid number"<< endl;
-            // cin.clear() gets rid of the user's input
-            // cin.ignore() removes the input from the buffer, well it removes 10000 characters 
-            cin.clear();
-            cin.ignore(10000, '\n');
+        catch (const invalid_argument&) {
+        cout << "Invalid input: not a number." << endl;
         }
     }
-    return number;
+    return binary_result;
 }
 int main() {
     cout << bitset<32>(get_number());
